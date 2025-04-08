db9a445434f2d68d1642f0402ec5c644212dcf45
saaad99 <ehdgnstla@naver.com>
2025-04-08 20:36:45 +0900

    [chore] FE/BE API통신 테스트 완료

---
+++ src/main/java/programo/_pro/controller/ApiTestController.java
@@ -0,0 +1,19 @@
+package programo._pro.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+@RequiredArgsConstructor
+public class ApiTestController {
+
+    @GetMapping("/data")
+    public String data(){
+        return "연결 테스트";
+    }
+}
