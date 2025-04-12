5dec1c057558791a409978a4cf59db55fff6e631
saaad99 <ehdgnstla@naver.com>
2025-04-12 19:14:46 +0900

    [Test] api/data 응답 텍스트 변경

--- src/main/java/programo/_pro/controller/ApiTestController.java
+++ src/main/java/programo/_pro/controller/ApiTestController.java
@@ -14,6 +14,6 @@ public class ApiTestController {
 
     @GetMapping("/data")
     public String data(){
-        return "연결 테스트 제발 돼라 자동화 제발요";
+        return "연결 테스트 너 아깐 안됐잖아 왜 되는척해 ? ";
     }
 }
