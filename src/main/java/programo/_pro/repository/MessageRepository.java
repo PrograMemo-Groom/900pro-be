37cd35a6000e3c3a2f27100473cda493c9c3878b
happygirlll <lbm000314@gmail.com>
2025-04-13 00:13:44 +0900

    [feat] MessageRepository 생성 - 채팅 메시지를 DB에 저장하고 불러오는 역할을 가짐

---
+++ src/main/java/programo/_pro/repository/MessageRepository.java
@@ -0,0 +1,10 @@
+package programo._pro.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import programo._pro.entity.Message;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+	List<Message> findByTeamIdOrderByCreatedAtAsc(Long teamId);
+}
