d7511891528b313de8d6f3800dc01ba0f206015e
yoorym-kim <twilight99me@gmail.com>
2025-04-15 22:44:06 +0900

    [feat] teamlist 정렬, 필터링 기능 추가

--- src/main/java/programo/_pro/controller/TeamController.java
+++ src/main/java/programo/_pro/controller/TeamController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import programo._pro.dto.TeamCardDto;
 import programo._pro.service.TeamService;
@@ -18,8 +19,11 @@ public class TeamController {
     private final TeamService teamService;
 
     @GetMapping
-    public ResponseEntity<List<TeamCardDto>> getAllTeams() {
-        List<TeamCardDto> teamCards = teamService.getAllTeams();
+    public ResponseEntity<List<TeamCardDto>> getAllTeams(
+            @RequestParam(value = "level", required = false) String level,
+            @RequestParam(value = "sort", required = false) String sort
+    ) {
+        List<TeamCardDto> teamCards = teamService.getAllTeams(level, sort);
         return ResponseEntity.ok(teamCards);
     }
 }
--- src/main/java/programo/_pro/service/TeamService.java
+++ src/main/java/programo/_pro/service/TeamService.java
@@ -6,6 +6,7 @@ import programo._pro.dto.TeamCardDto;
 import programo._pro.entity.Team;
 import programo._pro.repository.TeamRepository;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,8 +16,26 @@ public class TeamService {
 
     private final TeamRepository teamRepository;
 
-    public List<TeamCardDto> getAllTeams() {
+    public List<TeamCardDto> getAllTeams(String level, String sort) {
         List<Team> teams = teamRepository.findByIsActiveTrue();
+
+        // 난이도 선택 안할경우 all이 디폴트
+        if (level != null && !level.equalsIgnoreCase("all")) {
+            teams = teams.stream()
+                    .filter(team -> team.getLevel().name().equalsIgnoreCase(level))
+                    .collect(Collectors.toList());
+        }
+
+        // 정렬 선택 안할경우 최신순이 디폴트
+        if (sort == null || sort.isBlank() || sort.equalsIgnoreCase("createdAt")) {
+            teams.sort(Comparator.comparing(Team::getCreatedAt).reversed());
+        } else if ("problemCount".equals(sort)) {
+            teams.sort(Comparator.comparing(Team::getProblemCount).reversed());
+        } else if ("name".equals(sort)) {
+            teams.sort(Comparator.comparing(Team::getTeamName));
+        } else if ("currentMembers".equals(sort)) {
+            teams.sort(Comparator.comparing(Team::getCurrentMembers).reversed());
+        }
 
         return teams.stream()
                 .map(team -> TeamCardDto.builder()
