443f9fc01e953a78af5738ede68ab1ac595f76bb
happygirlll <lbm000314@gmail.com>
2025-04-16 16:52:05 +0900

    [feat] required = true를 추가해서 필수 입력값 명시

--- src/main/java/programo/_pro/dto/ChatMessageRequest.java
+++ src/main/java/programo/_pro/dto/ChatMessageRequest.java
@@ -9,13 +9,13 @@ import lombok.Setter;
 @Schema(description = "채팅 메시지 전송 요청")
 public class ChatMessageRequest {
 
-	@Schema(description = "채팅방 ID", example = "1")
+	@Schema(description = "채팅방 ID", example = "1", required = true)
 	private Long chatRoomId;
 
-	@Schema(description = "보낸 사람 ID", example = "10")
+	@Schema(description = "보낸 사람 ID", example = "10", required = true)
 	private Long userId;
 
-	@Schema(description = "메시지 내용", example = "안녕하세요 팀원 여러분!")
+	@Schema(description = "메시지 내용", example = "안녕하세요 팀원 여러분!", required = true)
 	private String content;
 
 
