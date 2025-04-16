788fb756be5f6d5d617dbb26993b773dd5dc54a2
saaad99 <ehdgnstla@naver.com>
2025-04-16 18:26:20 +0900

    [Chore] Mail 컨트롤러, 서비스 기능 - 인증 이메일 전송 테스트 완료 - 인증번호 입력 후 일치여부 확인 테스트 완료

---
+++ src/main/java/programo/_pro/controller/MailController.java
@@ -0,0 +1,45 @@
+package programo._pro.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import programo._pro.dto.EmailRequest;
+import programo._pro.service.MailService;
+
+import java.util.HashMap;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/mail")
+public class MailController {
+    private final MailService mailService;
+    private int number; // 이메일 인증 숫자를 저장하는 변수
+
+    // 인증 이메일 전송
+    @PostMapping("/mailSend")
+    public HashMap<String, Object> mailSend(@RequestBody EmailRequest email) {
+        HashMap<String, Object> map = new HashMap<>();
+
+        try {
+            number = mailService.sendMail(email);
+            String num = String.valueOf(number);
+
+            map.put("success", Boolean.TRUE);
+            map.put("number", num);
+        } catch (Exception e) {
+            map.put("success", Boolean.FALSE);
+            map.put("error", e.getMessage());
+        }
+
+        return map;
+    }
+
+    // 인증번호 일치여부 확인
+    @GetMapping("/mailCheck")
+    public ResponseEntity<?> mailCheck(@RequestParam String userNumber) {
+
+        boolean isMatch = userNumber.equals(String.valueOf(number));
+
+        return ResponseEntity.ok(isMatch);
+    }
+}
--- src/main/java/programo/_pro/service/MailService.java
+++ src/main/java/programo/_pro/service/MailService.java
@@ -1,4 +1,55 @@
 package programo._pro.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import programo._pro.dto.EmailRequest;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class MailService {
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    private final JavaMailSender javaMailSender;
+    private static int number;
+
+    // 랜덤으로 숫자 생성
+    public static void createNumber() {
+        number = (int)(Math.random() * (90000)) + 100000; //(int) Math.random() * (최댓값-최소값+1) + 최소값
+    }
+
+    public MimeMessage CreateMail(String email) {
+        createNumber();
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        try {
+            message.setFrom(username);
+            message.setRecipients(MimeMessage.RecipientType.TO, email);
+            message.setSubject("이메일 인증");
+            String body = "";
+            body += "<h3>" + "요청하신 인증 번호입니다." + "</h3>";
+            body += "<h1>" + number + "</h1>";
+            body += "<h3>" + "감사합니다." + "</h3>";
+            message.setText(body,"UTF-8", "html");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            log.error("예외 메시지: {}", e.getMessage());
+        }
+
+        return message;
+    }
+
+    public int sendMail(EmailRequest emailRequest) {
+        MimeMessage message = CreateMail(emailRequest.getEmail());
+        javaMailSender.send(message);
+
+        return number;
+    }
 }
