bf96105d2ee5115fe4af90af58d231d2305d108f
saaad99 <ehdgnstla@naver.com>
2025-04-16 21:56:22 +0900

    [Feature] 중복 체크 로직 추가 - 기존의 회원가입에 이메일 중복 여부 확인 코드 추가 - 별도의 입력받은 이메일 중복 여부 확인 함수 추가

--- src/main/java/programo/_pro/service/AuthService.java
+++ src/main/java/programo/_pro/service/AuthService.java
@@ -1,17 +1,14 @@
 package programo._pro.service;
 
-import programo._pro.dto.JwtUserInfoDto;
-import programo._pro.dto.UserInfo;
-import programo._pro.entity.User;
-import programo._pro.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.NoSuchElementException;
+import programo._pro.dto.DupCheckDto;
+import programo._pro.dto.UserInfo;
+import programo._pro.entity.User;
+import programo._pro.repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
@@ -23,8 +20,27 @@ public class AuthService {
 
     @Transactional
     public User signUp(UserInfo userInfo) {
+        // 이메일 중복 확인
+        boolean exists = userRepository.existsByEmail(userInfo.getEmail());
+        if (exists) {
+            throw new IllegalStateException("이미 사용 중인 이메일입니다.");
+        }
         String encryptedPassword = passwordEncoder.encode(userInfo.getPassword());
         return userRepository.save(userInfo.toEntity(encryptedPassword));
+    }
 
+
+    @Transactional
+    public boolean DupCheck(DupCheckDto checkDto) {
+        // 이메일 중복 확인
+        boolean exists = userRepository.existsByEmail(checkDto.getEmail());
+
+        // 이미 존재한다면
+        if (exists) {
+            return false;
+        }
+        // 사용 가능한 이메일일 때
+        return true;
     }
 }
+
