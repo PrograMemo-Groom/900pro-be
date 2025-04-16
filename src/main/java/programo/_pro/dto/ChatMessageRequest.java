a11d653ee24c73d3541103f37fa8ddd234dcaa19
happygirlll <lbm000314@gmail.com>
2025-04-16 09:47:11 +0900

    [feat] ChatMessageRequest에 컬럼 수정

--- src/main/java/programo/_pro/dto/ChatMessageRequest.java
+++ src/main/java/programo/_pro/dto/ChatMessageRequest.java
@@ -1,20 +1,22 @@
 package programo._pro.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
 
-@Schema(description = "채팅 메시지 전송 요청 DTO")
+@Getter
+@Setter
+@Schema(description = "채팅 메시지 전송 요청")
 public class ChatMessageRequest {
 
-	@Schema(description = "팀 ID", example = "1")
-	private Long teamId;
+	@Schema(description = "채팅방 ID", example = "1")
+	private Long chatRoomId;
 
 	@Schema(description = "보낸 사람 ID", example = "10")
-	private Long senderId;
+	private Long userId;
 
 	@Schema(description = "메시지 내용", example = "안녕하세요 팀원 여러분!")
-	private String message;
+	private String content;
 
-	public String getMessage() {
-		return "전송 완료";
-	}
+
 }
