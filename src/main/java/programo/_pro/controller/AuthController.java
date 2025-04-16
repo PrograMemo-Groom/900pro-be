542d8c62d5c3feaaf5a2c6221c3884b21982031b
saaad99 <ehdgnstla@naver.com>
2025-04-16 15:49:53 +0900

    [Refactor] '/login' 엔드포인트 처리 로직 제거
    - JwtAuthenticationFilter.java가 이 url에 대한 필터를 직접 지정하여 처리 중

--- src/main/java/programo/_pro/controller/AuthController.java
+++ src/main/java/programo/_pro/controller/AuthController.java
@@ -18,11 +18,6 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final AuthService authService;
 
-    @PostMapping("/login")
-    public ResponseEntity<ApiResponse<String>> signIn(@RequestBody @Valid SignInDto signInDto) {
-        return ResponseEntity.ok(ApiResponse.success(authService.signIn(signInDto.getEmail(), signInDto.getPassword())));
-    }
-
     @PostMapping("/join")
     public ResponseEntity<ApiResponse<User>> signUp(@RequestBody @Valid SignUpDto signUpDto) {
         return ResponseEntity.ok(ApiResponse.success(authService.signUp(signUpDto.toService())));
