ad0a4df685ffb51f6fa3a9233ae47890c13aebba
saaad99 <ehdgnstla@naver.com>
2025-04-16 09:37:11 +0900

    [Style] User 테이블 검증 추가 및 컬럼 수정 - 패스워드 패턴 및 사이즈 검증 추가 - 테이블에 없는 flag 컬럼 삭제 - isActive 컬럼 추가 - Email 검증 어노테이션 추가

--- src/main/java/programo/_pro/entity/User.java
+++ src/main/java/programo/_pro/entity/User.java
@@ -1,5 +1,8 @@
 package programo._pro.entity;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,19 +23,26 @@ public class User {
     @Column(name = "id")
     private Long id;
 
+    @Email
     @Column(name = "email", unique = true, nullable = false)
     private String email;
 
     @Column(name = "user_name", nullable = false)
     private String username;
 
+    // 한글, 공백, 이모지 자동 차단
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*?])[A-Za-z\\d~!@#$%^&*?]{8,20}$",
+            message = "비밀번호는 8~20자이며, 영문자, 숫자, ~!@#$%^&*? 중 하나 이상의 특수문자를 포함해야 합니다."
+    )
+    @Size(min = 8, max = 20, message = "비밀번호는 8~20자여야 합니다.")
     @Column(name = "password")
     private String password;
 
-    @Column(name = "flag")
-    private Integer flag;
+    @Column(name = "is_active", nullable = false)
+    private boolean isActive = true;
 
-    public boolean isActive() {
-        return flag == 1;
-    }
+//    public boolean isActive() {
+//        return flag == 1;
+//    }
 }
