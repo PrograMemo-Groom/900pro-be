a2b7c06998a101d0159e1d8d43947648c15b7220
happygirlll <lbm000314@gmail.com>
2025-04-16 16:51:00 +0900

    [feat] swagger 테스트를 위함

--- src/main/java/programo/_pro/controller/ChatController.java
+++ src/main/java/programo/_pro/controller/ChatController.java
@@ -1,5 +1,7 @@
 package programo._pro.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -14,6 +16,14 @@ public class ChatController {
 	private final ChatService chatService;
 
 	@MessageMapping("/chat/message") // /pub/chat/message
+	@Operation(
+			summary = "채팅 메시지 전송",
+			description = "사용자가 채팅방에 메시지를 전송하면 이를 처리하는 API입니다.",
+			requestBody = @RequestBody(
+					description = "전송할 채팅 메시지",
+					required = true
+			)
+	)
 	public void receiveMessage(@Payload ChatMessageRequest messageRequest) {
 		chatService.handleChatMessage(messageRequest);
 	}
