f8950655e26c344a3f6d0507c7e56ad71ffbebd7
Kim Keon-Young <diamondgonny@gmail.com>
2025-04-17 11:27:59 +0900

    [fix] 코드 실행 API 응답 형식 변경
    - 각 언어의 코드 실행 메서드에서 응답 타입을 CodeExecutionResponse에서 ApiResponse<CodeExecutionResponse>로 변경

--- src/main/java/programo/_pro/controller/CodeExecutorController.java
+++ src/main/java/programo/_pro/controller/CodeExecutorController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import programo._pro.dto.CodeExecutionRequest;
 import programo._pro.dto.CodeExecutionResponse;
 import programo._pro.service.executor.CodeExecutorService;
+import programo._pro.global.ApiResponse;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,32 +20,32 @@ public class CodeExecutorController {
     private final CodeExecutorService codeExecutorService;
 
     @PostMapping("/execute/python")
-    public ResponseEntity<CodeExecutionResponse> executePythonCode(@RequestBody CodeExecutionRequest request) {
+    public ResponseEntity<ApiResponse<CodeExecutionResponse>> executePythonCode(@RequestBody CodeExecutionRequest request) {
         CodeExecutionResponse result = codeExecutorService.executePythonCode(request.getCode());
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(ApiResponse.success(result));
     }
 
     @PostMapping("/execute/javascript")
-    public ResponseEntity<CodeExecutionResponse> executeJavaScriptCode(@RequestBody CodeExecutionRequest request) {
+    public ResponseEntity<ApiResponse<CodeExecutionResponse>> executeJavaScriptCode(@RequestBody CodeExecutionRequest request) {
         CodeExecutionResponse result = codeExecutorService.executeJavaScriptCode(request.getCode());
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(ApiResponse.success(result));
     }
 
     @PostMapping("/execute/java")
-    public ResponseEntity<CodeExecutionResponse> executeJavaCode(@RequestBody CodeExecutionRequest request) {
+    public ResponseEntity<ApiResponse<CodeExecutionResponse>> executeJavaCode(@RequestBody CodeExecutionRequest request) {
         CodeExecutionResponse result = codeExecutorService.executeJavaCode(request.getCode());
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(ApiResponse.success(result));
     }
 
     @PostMapping("/execute/cpp")
-    public ResponseEntity<CodeExecutionResponse> executeCppCode(@RequestBody CodeExecutionRequest request) {
+    public ResponseEntity<ApiResponse<CodeExecutionResponse>> executeCppCode(@RequestBody CodeExecutionRequest request) {
         CodeExecutionResponse result = codeExecutorService.executeCppCode(request.getCode());
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(ApiResponse.success(result));
     }
 
     @PostMapping("/execute/c")
-    public ResponseEntity<CodeExecutionResponse> executeCCode(@RequestBody CodeExecutionRequest request) {
+    public ResponseEntity<ApiResponse<CodeExecutionResponse>> executeCCode(@RequestBody CodeExecutionRequest request) {
         CodeExecutionResponse result = codeExecutorService.executeCCode(request.getCode());
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(ApiResponse.success(result));
     }
 }
