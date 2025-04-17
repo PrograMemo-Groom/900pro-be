64069c512c02627aa169e80d070c5d51348b0205
saaad99 <ehdgnstla@naver.com>
2025-04-17 16:44:29 +0900

    [Feat] 이메일 인증 테이블 생성

---
+++ src/main/java/programo/_pro/entity/EmailVerification.java
@@ -0,0 +1,43 @@
+package programo._pro.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "email_verification")
+@Entity
+public class EmailVerification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private int code;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "expired_at", nullable = false)
+    private LocalDateTime expiredAt;
+
+    @Column(name = "is_verified", nullable = false)
+    private boolean isVerified = false;
+
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now(); // 현재 시간
+        expiredAt = LocalDateTime.now().plusMinutes(10); // 만료시간 10분으로 설정
+    }
+}
