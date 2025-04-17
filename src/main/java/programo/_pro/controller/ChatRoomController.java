c527c35d5da3c4b0367cc4ea92a39758ab0ec04f
happygirlll <lbm000314@gmail.com>
2025-04-17 11:35:30 +0900

    [feat] 채팅 Swagger를 위해 controller에 어노테이션 포함 - @Tag : "Chat", "Chatroom", "Chatbot" API에 대한 태그를 추가하여 Swagger UI에서 각 API의 그룹화를 구현 - @Operation : 각 API에 대해 간단한 설명과 응답 코드 및 상태 코드 설명을 추가 - @Parameter : API 요청에 필요한 파라미터에 대해 설명을 추가 - @ApiResponse : 각 API 응답에 대해 성공/실패 코드 및 상태 메시지 추가

--- src/main/java/programo/_pro/controller/ChatRoomController.java
+++ src/main/java/programo/_pro/controller/ChatRoomController.java
@@ -1,5 +1,9 @@
 package programo._pro.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import programo._pro.entity.ChatRoom;
@@ -7,18 +11,36 @@ import programo._pro.service.ChatRoomService;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Chatroom", description = "채팅방 조회&생성 API")
 @RequestMapping("/api/chatroom")
 public class ChatRoomController {
 
 	private final ChatRoomService chatRoomService;
 
+	@Operation(summary = "채팅방 생성",
+			description = "새로운 채팅방을 생성합니다.",
+			responses = {
+					@ApiResponse(responseCode = "201", description = "채팅방 생성 성공"),
+					@ApiResponse(responseCode = "400", description = "잘못된 요청: 팀 ID가 존재하지 않거나 유효하지 않습니다."),
+					@ApiResponse(responseCode = "404", description = "팀을 찾을 수 없음")
+			})
 	@PostMapping
-	public ChatRoom createChatRoom(@RequestBody Long teamId) {
+	public ChatRoom createChatRoom(
+			@Parameter(description = "새로 생성할 팀 ID")
+			@RequestParam Long teamId) {
 		return chatRoomService.createChatRoom(teamId);
 	}
 
+	@Operation(summary = "팀 ID로 채팅방 조회",
+			description = "특정 팀 ID에 해당하는 채팅방을 조회합니다.",
+			responses = {
+					@ApiResponse(responseCode = "200", description = "채팅방 조회 성공"),
+					@ApiResponse(responseCode = "404", description = "채팅방을 찾을 수 없음")
+			})
 	@GetMapping("/{teamId}")
-	public ChatRoom getChatRoom(@PathVariable Long teamId) {
+	public ChatRoom getChatRoomByTeamId(
+			@Parameter(description = "조회할 팀의 ID")
+			@PathVariable Long teamId) {
 		return chatRoomService.getChatRoomByTeamId(teamId);
 	}
 }
