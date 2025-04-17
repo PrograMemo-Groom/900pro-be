9d7d63b6c121bac4aacac331763510493f75e6d7
saaad99 <ehdgnstla@naver.com>
2025-04-17 17:40:35 +0900

    [Feat] 이메일 인증 레포 생성

--- src/main/java/programo/_pro/repository/EmailVerificationRepository.java
+++ src/main/java/programo/_pro/repository/EmailVerificationRepository.java
@@ -1,7 +1,12 @@
 package programo._pro.repository;
 
+import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 import programo._pro.entity.EmailVerification;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Integer> {
+    Optional<EmailVerification> findByEmail(@Email String email);
 }
