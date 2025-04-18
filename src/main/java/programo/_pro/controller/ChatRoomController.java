fea3d5b5960ae0c04d8fafced39e01af1170b450
happygirlll <lbm000314@gmail.com>
2025-04-18 15:57:09 +0900

    [feat] ChatRoomController swagger ui를 위해 설명 추가, dto 연결

--- src/main/java/programo/_pro/controller/ChatRoomController.java
+++ src/main/java/programo/_pro/controller/ChatRoomController.java
@@ -2,10 +2,12 @@ package programo._pro.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import programo._pro.dto.ChatRoomRequest;
 import programo._pro.entity.ChatRoom;
 import programo._pro.service.ChatRoomService;
 
@@ -18,7 +20,7 @@ public class ChatRoomController {
 	private final ChatRoomService chatRoomService;
 
 	@Operation(summary = "채팅방 생성",
-			description = "새로운 채팅방을 생성합니다.",
+			description = "팀 생성과 함께, 새로운 채팅방을 생성합니다.",
 			responses = {
 					@ApiResponse(responseCode = "201", description = "채팅방 생성 성공"),
 					@ApiResponse(responseCode = "400", description = "잘못된 요청: 팀 ID가 존재하지 않거나 유효하지 않습니다."),
@@ -27,8 +29,8 @@ public class ChatRoomController {
 	@PostMapping
 	public ChatRoom createChatRoom(
 			@Parameter(description = "새로 생성할 팀 ID")
-			@RequestParam Long teamId) {
-		return chatRoomService.createChatRoom(teamId);
+			@RequestBody ChatRoomRequest chatRoomRequest) {
+		return chatRoomService.createChatRoom(chatRoomRequest.getTeamId());
 	}
 
 	@Operation(summary = "팀 ID로 채팅방 조회",
@@ -37,6 +39,7 @@ public class ChatRoomController {
 					@ApiResponse(responseCode = "200", description = "채팅방 조회 성공"),
 					@ApiResponse(responseCode = "404", description = "채팅방을 찾을 수 없음")
 			})
+
 	@GetMapping("/{teamId}")
 	public ChatRoom getChatRoomByTeamId(
 			@Parameter(description = "조회할 팀의 ID")
