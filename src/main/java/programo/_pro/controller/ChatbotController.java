4479054ddab0841767b5c382a6d8ce8d6e5605c5
happygirlll <lbm000314@gmail.com>
2025-04-18 15:56:06 +0900

    [feat] ChatbotController 다듬기

--- src/main/java/programo/_pro/controller/ChatbotController.java
+++ src/main/java/programo/_pro/controller/ChatbotController.java
@@ -1,10 +1,12 @@
 package programo._pro.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import programo._pro.dto.ChatbotRequest;
 import programo._pro.entity.Chatbot;
 import programo._pro.service.ChatbotService;
 
@@ -18,18 +20,6 @@ public class ChatbotController {
 
 	private final ChatbotService chatbotService;
 
-	@Operation(summary = "팀 ID로 챗봇 메시지 조회",
-			description = "팀 ID에 속한 챗봇 메시지 목록을 조회합니다.",
-			responses = {
-					@ApiResponse(responseCode = "200", description = "챗봇 메시지 조회 성공"),
-					@ApiResponse(responseCode = "404", description = "팀을 찾을 수 없음 : 해당 팀에 속하는 챗봇 메시지를 찾을 수 없습니다."),
-					@ApiResponse(responseCode = "500", description = "서버 오류: 메시지 조회 중 문제가 발생했습니다.")
-			})
-	@GetMapping("/{teamId}")
-	public List<Chatbot> getChatbotsByTeamId(@PathVariable Long teamId) {
-		return chatbotService.getChatbotsByTeamId(teamId);
-	}
-
 	@Operation(summary = "새로운 챗봇 메시지 생성",
 			description = "새로운 챗봇 메시지를 생성합니다.",
 			responses = {
@@ -38,7 +28,24 @@ public class ChatbotController {
 					@ApiResponse(responseCode = "500", description = "서버 오류: 메시지 생성 중 문제가 발생했습니다.")
 			})
 	@PostMapping
-	public void createChatbotMessage(@RequestBody Chatbot chatbot) {
-		chatbotService.createChatbotMessage(chatbot);
+	public void createChatbotMessage(
+			@Parameter(description = "챗봇 메시지 생성 요청 데이터", required = true)
+			@RequestBody ChatbotRequest chatbotRequest) {
+		chatbotService.createChatbotMessage(chatbotRequest);
 	}
+
+	@Operation(summary = "팀 ID로 챗봇 메시지 조회",
+			description = "팀 ID에 속한 챗봇 메시지 목록을 조회합니다.",
+			responses = {
+					@ApiResponse(responseCode = "200", description = "챗봇 메시지 조회 성공"),
+					@ApiResponse(responseCode = "404", description = "팀을 찾을 수 없음 : 해당 팀에 속하는 챗봇 메시지를 찾을 수 없습니다."),
+					@ApiResponse(responseCode = "500", description = "서버 오류: 메시지 조회 중 문제가 발생했습니다.")
+			})
+	@GetMapping("/{teamId}")
+	public List<Chatbot> getChatbotsByTeamId(
+			@Parameter(description = "조회할 팀의 ID", required = true)
+			@PathVariable Long teamId) {
+		return chatbotService.getChatbotsByTeamId(teamId);
+	}
+
 }
