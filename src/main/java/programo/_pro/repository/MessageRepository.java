629e4312e0474356d8218e4aa037edf5debf4644
happygirlll <lbm000314@gmail.com>
2025-04-19 16:11:23 +0900

    [feat] MessageRepository에 날짜 범위 기준으로 메시지 조회하는 메서드 추가

--- src/main/java/programo/_pro/repository/MessageRepository.java
+++ src/main/java/programo/_pro/repository/MessageRepository.java
@@ -3,9 +3,13 @@ package programo._pro.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import programo._pro.entity.Message;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
 	List<Message> findByChatRoom_Id(Long chatRoomId);
 	List<Message> findByChatRoom_IdOrderBySendAtAsc(Long chatRoomId);
+
+	// 특정 날짜 범위에서 메시지를 조회
+	List<Message> findByChatRoom_TeamIdAndSendAtBetween(Long teamId, LocalDateTime start, LocalDateTime end);
 }
