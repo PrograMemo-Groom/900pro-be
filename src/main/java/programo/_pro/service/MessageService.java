33ae684c27f7e8079896c370032b2f54297addbb
happygirlll <lbm000314@gmail.com>
2025-04-19 16:37:42 +0900

    [feat] MessageService 검색 메서드 추가

--- src/main/java/programo/_pro/service/MessageService.java
+++ src/main/java/programo/_pro/service/MessageService.java
@@ -24,4 +24,17 @@ public class MessageService {
 		// 주어진 날짜 범위에 해당하는 메시지들 가져오기
 		return messageRepository.findByChatRoom_TeamIdAndSendAtBetween(teamId, startOfDay, endOfDay);
 	}
+
+	// 키워드로 메시지를 검색하는 메서드
+	public List<Message> searchMessagesByKeyword(Long chatRoomId, String keyword) {
+		// 검색된 메시지를 최신순으로 가져오기
+		List<Message> messages = messageRepository.findByChatRoom_IdAndContentContainingOrderBySendAtDesc(chatRoomId, keyword);
+
+		for (Message message : messages) {
+			message.setContent(message.getContent().replaceAll("(?i)" + keyword, "<span style='color:yellow;'>" + keyword + "</span>"));
+		}
+		return messages;
+	}
+
+
 }
