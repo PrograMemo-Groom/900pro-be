898f2bed23c96f274011ab7c516d92a2501a6173
happygirlll <lbm000314@gmail.com>
2025-04-19 16:38:07 +0900

    [feat] MessageRepository 검색 메시지 정렬

--- src/main/java/programo/_pro/repository/MessageRepository.java
+++ src/main/java/programo/_pro/repository/MessageRepository.java
@@ -12,4 +12,7 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
 
 	// 특정 날짜 범위에서 메시지를 조회
 	List<Message> findByChatRoom_TeamIdAndSendAtBetween(Long teamId, LocalDateTime start, LocalDateTime end);
+
+	// 검색된 메시지가 최신 메시지부터 정렬되도록
+	List<Message> findByChatRoom_IdAndContentContainingOrderBySendAtDesc(Long chatRoomId, String keyword);
 }
