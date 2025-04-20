390821f9b37fc5b33af0c8c5a9039a7daf8850c9
saaad99 <ehdgnstla@naver.com>
2025-04-20 18:20:55 +0900

    [Feat] 회원정보업데이트 기능 구현

--- src/main/java/programo/_pro/controller/MyPageController.java
+++ src/main/java/programo/_pro/controller/MyPageController.java
@@ -1,15 +1,14 @@
 package programo._pro.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import programo._pro.dto.userDto.UserDto;
-import programo._pro.dto.userDto.UserUpdateRequestDto;
-import programo._pro.entity.User;
+import programo._pro.dto.userDto.UserInfoUpdateDto;
 import programo._pro.global.ApiResponse;
-import programo._pro.global.exception.userException.NotFoundUserException;
 import programo._pro.service.MyPageService;
 
 @RestController
@@ -29,13 +28,14 @@ public class MyPageController {
         return ResponseEntity.ok(ApiResponse.success(userDto, String.format("userId : %d 의 회원 정보를 조회했습니다.", userId)));
     }
 
-    //
-//    // 유저 id 를 받고, body 로 들어온 정보로 업데이트
-//    @PatchMapping("/update")
-//    public ResponseEntity<ApiResponse<String>> updateUserInfo(@RequestParam long userId, @RequestBody User user) {
-//
-//
-//    }
+
+    // 유저 id 를 받고, body 로 들어온 정보로 업데이트
+    @PatchMapping("/update/{userId}")
+    public ResponseEntity<ApiResponse<String>> updateUserInfo(@PathVariable int userId, @RequestBody @Valid UserInfoUpdateDto userInfoUpdateDto) {
+         myPageService.updateUserInfo(userId, userInfoUpdateDto);
+
+         return ResponseEntity.ok(ApiResponse.success("success", "성공적으로 회원정보를 업데이트했습니다."));
+    }
 
 
 }
