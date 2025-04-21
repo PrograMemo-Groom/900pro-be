1e1eb55c38e19041e5c4810052dfeb8aceef48c8
saaad99 <ehdgnstla@naver.com>
2025-04-21 15:20:08 +0900

    [Comment] history 관련 주석 처리 - 기능 개발이 다 안된 것을 배포서버에 올려버림

--- src/main/java/programo/_pro/controller/HistoryController.java
+++ src/main/java/programo/_pro/controller/HistoryController.java
@@ -1,26 +1,26 @@
-package programo._pro.controller;
-
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import programo._pro.global.ApiResponse;
-import programo._pro.service.HistoryService;
-
-@RestController
-@RequestMapping("/api/history")
-@Slf4j
-@RequiredArgsConstructor
-@Tag(name = "히스토리 페이지", description = "히스토리 페이지 API")
-public class HistoryController {
-    private final HistoryService historyService;
-
-    @GetMapping("/{testId}")
-    public ResponseEntity<ApiResponse<String>> getHistory(@PathVariable String testId) {
-        historyService.getHistory(testId);
-    }
-}
+//package programo._pro.controller;
+//
+//import io.swagger.v3.oas.annotations.tags.Tag;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import programo._pro.global.ApiResponse;
+//import programo._pro.service.HistoryService;
+//
+//@RestController
+//@RequestMapping("/api/history")
+//@Slf4j
+//@RequiredArgsConstructor
+//@Tag(name = "히스토리 페이지", description = "히스토리 페이지 API")
+//public class HistoryController {
+//    private final HistoryService historyService;
+//
+//    @GetMapping("/{testId}")
+//    public ResponseEntity<ApiResponse<String>> getHistory(@PathVariable String testId) {
+//        historyService.getHistory(testId);
+//    }
+//}
--- src/main/java/programo/_pro/service/HistoryService.java
+++ src/main/java/programo/_pro/service/HistoryService.java
@@ -1,10 +1,10 @@
-package programo._pro.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class HistoryService {
-    public void getHistory(String testId) {
-
-    }
-}
+//package programo._pro.service;
+//
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class HistoryService {
+//    public void getHistory(String testId) {
+//
+//    }
+//}
