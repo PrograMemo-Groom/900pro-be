5a73dcbde2716990719c6432a2838fa3832f18ae
saaad99 <ehdgnstla@naver.com>
2025-04-22 02:06:08 +0900

    [Chore] CorsConfig 추가

---
+++ src/main/java/programo/_pro/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package programo._pro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true); // 쿠키 허용
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // 허용할 Origin
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
+        config.setExposedHeaders(Arrays.asList("Authorization")); // 클라이언트에서 읽을 수 있는 헤더
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
