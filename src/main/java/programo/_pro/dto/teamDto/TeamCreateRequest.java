257d17bd8756e1853daf48525581406a64605cf6
yoorym-kim <twilight99me@gmail.com>
2025-04-23 15:37:47 +0900

    [fix] TeamCreateRequest에 각 input창(teamname, teamdetail)에 대한 길이제한 추가

--- src/main/java/programo/_pro/dto/teamDto/TeamCreateRequest.java
+++ src/main/java/programo/_pro/dto/teamDto/TeamCreateRequest.java
@@ -1,10 +1,7 @@
 package programo._pro.dto.teamDto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import programo._pro.entity.Level;
@@ -16,10 +13,12 @@ import java.time.LocalDateTime;
 public class TeamCreateRequest {
 
     @Schema(description = "팀 이름", example = "코테 참 잘하는 팀")
-    @NotBlank
+    @NotBlank(message = "팀 이름은 필수로 기입해야 합니다")
+    @Size(min = 2, max = 10, message = "팀 이름은 2자 이상 10자 이하로 입력해주세요")
     private String teamName;
 
     @Schema(description = "팀 설명", example = "안녕하세요 “프로그램(Program)”과 “Memo(기억, 기록)”를 합쳐, 함께 개발하며 기억에 남는 성과를 만들어가는  팀 “프로그래모(PrograMemo)” 입니다")
+    @Size(max = 700, message = "팀 설명은 적지 않거나, 최대 700자까지 입력 가능합니다")
     private String description;
 
     @Schema(description = "팀 난이도", example = "하", implementation = Level.class)
