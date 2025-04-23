4137c64144e228e0f43afbfed56a7e50ad410482
yoorym-kim <twilight99me@gmail.com>
2025-04-23 15:39:03 +0900

    [fix] TeamCreateRequest의 Swagger 문서용 @Schema에 추가된 길이제한 명시

--- src/main/java/programo/_pro/dto/teamDto/TeamCreateRequest.java
+++ src/main/java/programo/_pro/dto/teamDto/TeamCreateRequest.java
@@ -12,12 +12,12 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class TeamCreateRequest {
 
-    @Schema(description = "팀 이름", example = "코테 참 잘하는 팀")
+    @Schema(description = "팀 이름 (2 ~ 10 자)", example = "코테 참 잘하는 팀")
     @NotBlank(message = "팀 이름은 필수로 기입해야 합니다")
     @Size(min = 2, max = 10, message = "팀 이름은 2자 이상 10자 이하로 입력해주세요")
     private String teamName;
 
-    @Schema(description = "팀 설명", example = "안녕하세요 “프로그램(Program)”과 “Memo(기억, 기록)”를 합쳐, 함께 개발하며 기억에 남는 성과를 만들어가는  팀 “프로그래모(PrograMemo)” 입니다")
+    @Schema(description = "팀 설명 (최대 700 자, 생략 가능)", example = "안녕하세요 “프로그램(Program)”과 “Memo(기억, 기록)”를 합쳐, 함께 개발하며 기억에 남는 성과를 만들어가는  팀 “프로그래모(PrograMemo)” 입니다")
     @Size(max = 700, message = "팀 설명은 적지 않거나, 최대 700자까지 입력 가능합니다")
     private String description;
 
