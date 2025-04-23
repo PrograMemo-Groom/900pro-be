0532bcd14fa221224d41c4a9062ce8938d5867a8
saaad99 <ehdgnstla@naver.com>
2025-04-23 16:27:38 +0900

    [Feat] GlobalExceptionHandler 에 예외 추가 - ProblemException 추가 - TestException 추가 - HighlightException 추가

--- src/main/java/programo/_pro/global/exception/GlobalExceptionHandler.java
+++ src/main/java/programo/_pro/global/exception/GlobalExceptionHandler.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import programo._pro.global.ApiResponse;
 import programo._pro.global.exception.chatException.NotFoundChatException;
+import programo._pro.global.exception.highlightException.HighlightException;
+import programo._pro.global.exception.problemException.ProblemException;
+import programo._pro.global.exception.testException.TestException;
 import programo._pro.global.exception.userException.UserException;
 
 @Slf4j
@@ -104,5 +107,25 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
+    // 문제 관련 예외 처리
+    @ExceptionHandler(ProblemException.class)
+    public ResponseEntity<ApiResponse<?>> handleProblemException(ProblemException ex) {
+        ApiResponse<?> response = ApiResponse.fail(ex.getMessage());
+        log.error(ex.getMessage(), ex);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
+    @ExceptionHandler(TestException.class)
+    public ResponseEntity<ApiResponse<?>> handleTestException(TestException ex) {
+        ApiResponse<?> response = ApiResponse.fail(ex.getMessage());
+        log.error(ex.getMessage(), ex);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(HighlightException.class)
+    public ResponseEntity<ApiResponse<?>> handleHighlightException(HighlightException ex) {
+        ApiResponse<?> response = ApiResponse.fail(ex.getMessage());
+        log.error(ex.getMessage(), ex);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
