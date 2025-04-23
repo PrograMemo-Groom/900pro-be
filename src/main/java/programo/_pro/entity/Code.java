bf3168becdec193135bca94677863b69d94a88d9
saaad99 <ehdgnstla@naver.com>
2025-04-23 16:19:50 +0900

    [Feat] Code 엔티티 추가

--- src/main/java/programo/_pro/entity/Code.java
+++ src/main/java/programo/_pro/entity/Code.java
@@ -1,34 +1,48 @@
-//package programo._pro.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Table(name = "code")
-//@Entity
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Data
-//public class Code {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private long id;
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @Column(name = "test_id", nullable = false)
-//    private Test test;
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @Column(name = "problem_id", nullable = false)
-//    private Problem problem;
-//
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @Column(name = "user_id", nullable = false)
-//    private User user;
-//
-//
-//
-//}
+package programo._pro.entity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table(name = "code")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Code {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "test_id", nullable = false)
+    private Test test;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "problem_id", nullable = false)
+    private Problem problem;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "language", nullable = false)
+    private String language;
+
+    @Column(name = "submit_code")
+    private String submitCode;
+
+    @Column(name = "submit_at")
+    private LocalDateTime submitAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    @Schema(description = "응시자 상태", example = "IN_PROGRESS, COMPLETED, ABSENT")
+    private Status status;
+}
