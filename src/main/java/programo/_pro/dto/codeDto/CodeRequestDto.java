06be8801826603494e37215890d9565538e7a65b
saaad99 <ehdgnstla@naver.com>
2025-04-23 21:44:22 +0900

    [Feat] 코드 요청 DTO

---
+++ src/main/java/programo/_pro/dto/codeDto/CodeRequestDto.java
@@ -0,0 +1,13 @@
+package programo._pro.dto.codeDto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class CodeRequestDto {
+    private int test_id;
+    private int problem_id;
+}
