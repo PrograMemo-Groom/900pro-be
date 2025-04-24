1cd5db8b572cc311b95ffde3066884a52142a5d8
saaad99 <ehdgnstla@naver.com>
2025-04-25 01:43:27 +0900

    [Feat] 코드 컨트롤러 - 기존의 CodeExecutorController.java 를 코드 컨트롤러에 합침 - 요청 주소는 그대로이므로 영향x - 시험을 완료한 사용자의 제출코드 상태를 모두 시험 완료로 수정하는 기능 구현

---
+++ src/main/java/programo/_pro/controller/CodeController.java
@@ -0,0 +1,65 @@
+package programo._pro.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import programo._pro.dto.codeDto.CodeExecutionRequest;
+import programo._pro.dto.codeDto.CodeExecutionResponse;
+import programo._pro.dto.codeDto.CodeRequestDto;
+import programo._pro.entity.Code;
+import programo._pro.global.ApiResponse;
+import programo._pro.service.CodeService;
+import programo._pro.service.executor.CodeExecutorService;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/code")
+public class CodeController {
+    private final CodeExecutorService codeExecutorService;
+    private final CodeService codeService;
+
+    // user_id, test_id를 입력받아 해당 유저의 문제풀이들을 시험완료 상태로 업데이트
+    @PostMapping("/update")
+    public ResponseEntity<ApiResponse<String>> updateSubmitCode(@RequestBody CodeRequestDto codeRequestDto) {
+        codeService.updateSubmitCode(codeRequestDto);
+
+        return ResponseEntity.ok(ApiResponse.success("해당 유저의 코드가 정상적으로 시험완료 상태로 변경되었습니다."));
+    }
+
+
+    @PostMapping("/execute/python")
+    public ResponseEntity<ApiResponse<CodeExecutionResponse>> executePythonCode(@RequestBody CodeExecutionRequest request) {
+        CodeExecutionResponse result = codeExecutorService.executePythonCode(request.getCode());
+        return ResponseEntity.ok(ApiResponse.success(result));
+    }
+
+    @PostMapping("/execute/javascript")
+    public ResponseEntity<ApiResponse<CodeExecutionResponse>> executeJavaScriptCode(@RequestBody CodeExecutionRequest request) {
+        CodeExecutionResponse result = codeExecutorService.executeJavaScriptCode(request.getCode());
+        return ResponseEntity.ok(ApiResponse.success(result));
+    }
+
+    @PostMapping("/execute/java")
+    public ResponseEntity<ApiResponse<CodeExecutionResponse>> executeJavaCode(@RequestBody CodeExecutionRequest request) {
+        CodeExecutionResponse result = codeExecutorService.executeJavaCode(request.getCode());
+        return ResponseEntity.ok(ApiResponse.success(result));
+    }
+
+    @PostMapping("/execute/cpp")
+    public ResponseEntity<ApiResponse<CodeExecutionResponse>> executeCppCode(@RequestBody CodeExecutionRequest request) {
+        CodeExecutionResponse result = codeExecutorService.executeCppCode(request.getCode());
+        return ResponseEntity.ok(ApiResponse.success(result));
+    }
+
+    @PostMapping("/execute/c")
+    public ResponseEntity<ApiResponse<CodeExecutionResponse>> executeCCode(@RequestBody CodeExecutionRequest request) {
+        CodeExecutionResponse result = codeExecutorService.executeCCode(request.getCode());
+        return ResponseEntity.ok(ApiResponse.success(result));
+    }
+}
