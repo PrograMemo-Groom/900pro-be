1719ea03026c4deef65f939cfb813ba9b1c22512
saaad99 <ehdgnstla@naver.com>
2025-04-24 23:13:00 +0900

    [Feat] Application.java @EnableScheduling 추가 - 스케쥴링 작동하도록 설정

--- src/main/java/programo/_pro/Application.java
+++ src/main/java/programo/_pro/Application.java
@@ -3,11 +3,13 @@ package programo._pro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.lang.NonNull;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
