dee0476385ffbd99adb5f535e57fbf1da59ac839
saaad99 <ehdgnstla@naver.com>
2025-04-29 22:33:30 +0900

    [Try?] 웹소켓에 인터셉터 등록

--- src/main/java/programo/_pro/config/WebSocketConfig.java
+++ src/main/java/programo/_pro/config/WebSocketConfig.java
@@ -2,10 +2,12 @@ package programo._pro.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import programo._pro.config.jwt.JwtChannelInterceptor;
 import programo._pro.config.jwt.JwtHandshakeInterceptor;
 import programo._pro.config.jwt.JwtTokenProvider;
 
@@ -15,6 +17,7 @@ import programo._pro.config.jwt.JwtTokenProvider;
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 //	private final JwtTokenProvider jwtTokenProvider;
+private final JwtChannelInterceptor jwtChannelInterceptor;
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -29,5 +32,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
 		registry.enableSimpleBroker("/sub"); // 메모리 기반 브로커
 		registry.setApplicationDestinationPrefixes("/pub");
+	}
+
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.interceptors(jwtChannelInterceptor); // ✅ 여기서 JwtChannelInterceptor 추가
 	}
 }
